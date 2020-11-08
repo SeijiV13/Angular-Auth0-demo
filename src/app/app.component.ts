e00826f7439a3c89a046e9d4336e7a53106cdd03
Seiji Ralph Villafranca <seijiv13@outlook.com>
2020-11-08 18:05:22 +0800

    Added auth 0 dependdencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-auth0-demo';
+  authenticated = false;
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {
+
+
+  }
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.subscribe(data => {
+      console.log(data);
+      if(!data) {
+        this.auth.loginWithRedirect();
+
+      }
+    });
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     // Import the module into the application, with configuration
+     AuthModule.forRoot({
+      domain: 'dev-9wq4ue47.us.auth0.com',
+      clientId: 'pXzZ8J4lv05H4jYe3dfqQvR0Z66o7OgK',
+      redirectUri: 'http://localhost:4200',
+      scope: 'openid'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
